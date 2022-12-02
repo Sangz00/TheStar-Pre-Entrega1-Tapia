c1208efd329a9fc5af30d94cc2cd7a322c77b4e9
Ruben David <rubendavid60@gmail.com>
2022-12-01 23:57:19 -0300

    commit

---
+++ src/components/Home.js
@@ -0,0 +1,11 @@
+import Card from './Card';
+
+function Home() {
+  return (
+    <div className="home_container">
+      Home
+    </div>
+  );
+}
+
+export default Home;
---
+++ src/components/NavBar.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export const NavBar = () => {
+  return (
+    <nav>
+      <ul>
+        <li><a>Inicio</a></li>
+        <li><a>Productos</a></li>
+        <li><a>Contacto</a></li>
+        <li><a>FAQ</a></li>
+        <li><a>Iniciar Sesion</a></li>
+        <li><a>Registrarse</a></li>
+      </ul>
+    </nav>
+  )
+}
